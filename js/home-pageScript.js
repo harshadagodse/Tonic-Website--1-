5f9ee0341b410073f05f7d7d4d2c3a290eb5ccf2
harshada <harshadagodse@staticgsm.com>
2024-03-13 17:18:04 +0530

    index.html, script, css

--- js/home-pageScript.js
+++ js/home-pageScript.js
@@ -1,23 +1,74 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 
+// let t1 = gsap.timeline({
+    
+//   scrollTrigger:{
+//     trigger:".team-members",
+//     start:"1% 50%",
+//     end:"120% 50%",
+//     markers:true,
+//     scrub:true,
+//   }
+  
+// });
+// console.log("Timeline created successfully" , t1);
+// t1.from(".exceptional-talent-meets-hover",{
+//   width:"0%",
+  
+  
+// })
+
+// document.addEventListener("DOMContentLoaded",()=>{
+//   gsap.registerPLugin(ScrollTrigger);
+//   new SplitType(".team-members",{
+//     types:"words"
+//   });
+//   $(".word").append("<div class='exceptional-talent-meets-hover'></div>");
+//   createAnimation();
+// })
+// function createAnimation() {
+//   let allMasks= $("..word").map(function(){
+//     return $(this).find((".exceptional-talent-meets-hover"));
+//     }).get();
+  
+// }
 let t1 = gsap.timeline({
     
   scrollTrigger:{
     trigger:".team-members",
-    start:"1% 50%",
-    end:"120% 50%",
+    start:"top 50%",
+    end:"bottom 50%",
     markers:true,
-    scrub:true,
-  }
+    scrub:1,
+  },
   
 });
 console.log("Timeline created successfully" , t1);
-t1.from(".exceptional-talent-meets-hover",{
-  width:"0%",
+t1.to(".exceptional-talent-meets-hover",{
+  width:"100%",
+  
   
   
 })
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".text p", {
+  backgroundPositionX: "200%",
+  stagger: 1,
+  scrollTrigger: {
+    trigger: ".text",
+    markers: true,
+    scrub: 1,
+    start: "1% 50%",
+    end: "120% 50%",
+  }
+});
+
+
+
+
+
 // gsap.registerPlugin(ScrollTrigger);
 
 // let t1 = gsap.timeline({
