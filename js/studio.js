1a3f50d95646fec32d13bf18e00b7ab3bf0a21d5
StaticGSM <gsm772@gmail.com>
2024-03-14 23:34:22 +0530

    base working pinned scroll

--- js/studio.js
+++ js/studio.js
@@ -1,66 +1,19 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector("#studio"),
-// smooth: true,
-// lerp:0.1,
-// tablet: { smooth: true },
-// smartphone: { smooth: true }
-// });
-
 const details = gsap.utils.toArray(".text-frame-with-info:not(:first-child)")
 const photos = gsap.utils.toArray(".image-rectangle:not(:first-child)")
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);  
 
-gsap.to(".text p", {
-  backgroundPositionX: "0%",
-  stagger: 1,
-  scrollTrigger: {
-    trigger: ".text",
-    markers: true,
-    scrub: 1,
-    start: "top center",
-    end: "bottom center"
-  }
-});
-gsap.to(".text1 p", {
-    backgroundPositionX: "0%",
-    stagger: 1,
-    scrollTrigger: {
-      trigger: ".text1",
-      markers: true,
-      scrub: 1,
-      start: "top center",
-      end: "bottom center"
-    }
-  });
-
-  gsap.to(".text2 p", {
-    backgroundPositionX: "0%",
-    stagger: 1,
-    scrollTrigger: {
-      trigger: ".text2",
-      markers: true,
-      scrub: 1,
-      start: "top center",
-      end: "bottom center"
-    }
-  });
-  
-
-gsap.set(photos, {yPercent:101})
+gsap.set(photos, {yPercent:100, opacity:0})
 
 const allPhotos = gsap.utils.toArray(".image-rectangle")
 
 
-// create
-let mm = gsap.matchMedia();
+// // create
+// let mm = gsap.matchMedia();
 
-// add a media query. When it matches, the associated function will run
-mm.add("(min-width: 600px)", () => {
-
+// // add a media query. When it matches, the associated function will run
+// mm.add("(min-width: 600px)", () => {
   // this setup code only runs when viewport is at least 600px wide
-  console.log("desktop")
-	
   ScrollTrigger.create({
 	trigger:".explore-frames",
 	start:"top top",
@@ -79,9 +32,9 @@ details.forEach((detail, index)=> {
     console.log(headline);
     console.log(photos[index]);
 	let animation = gsap.timeline()
-       .to(photos[index], { autoAlpha: 1 }) 
-        .to(photos[index], { yPercent: 0 }, 0.1)
-        .set(allPhotos[index], {autoAlpha:0});
+        .to(photos[index], { yPercent: 0, opacity:1})
+        .set(allPhotos[index], {opacity:1});
+    
 	ScrollTrigger.create({
 		trigger:headline,
 		start:"top 80%",
@@ -90,26 +43,9 @@ details.forEach((detail, index)=> {
 		scrub:true,
 		markers:true
 	})
-    // gsap.fromTo(heading, 
-    //     {opacity: 0},
-    //     {
-    //      opacity: 1,
-    //      ease: 'power1.inOut',
-    //      scrollTrigger: {
-    //          trigger: sections[i],
-    //          pin: 'main',
-    //        pinnedContainer: "main",
-    //          start: 'center center',
-    //          end: `+=${i + 1}00%`,
-    //          scrub: true,
-    //      },
-    //  })
+
 })
-	
-	
-  
-  return () => { // optional
-    // custom cleanup code here (runs when it STOPS matching)
-	  console.log("mobile")
-  };
-});
+
+//});
+
+
