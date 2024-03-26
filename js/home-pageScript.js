eedd26191c9997340accf3bb1218423ed5574ab1
StaticGSM <gsm772@gmail.com>
2024-03-26 13:20:00 +0530

    homepage and contact page map

--- js/home-pageScript.js
+++ js/home-pageScript.js
@@ -33,23 +33,21 @@ window.addEventListener('scroll', function() {
 //   this.classList.remove('hover-slow');
 // });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const frameContainer = document.querySelector('.frame-container');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const frameContainer = document.querySelector('.frame-container');
 
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-      frameContainer.classList.add('scrolled');
-    } else {
-      frameContainer.classList.remove('scrolled');
-    }
-  });
-});
+//   window.addEventListener('scroll', function() {
+//     if (window.scrollY > 0) {
+//       frameContainer.classList.add('scrolled');
+//     } else {
+//       frameContainer.classList.remove('scrolled');
+//     }
+//   });
+// });
 
 gsap.config({ trialWarn: false });
-console.clear();
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const split = new SplitText("p", { type: "lines" });
-console.log("split.lines", split.lines);
 split.lines.forEach((target) => {
   gsap.to(target, {
     backgroundPositionX: 0,
@@ -70,7 +68,7 @@ split.lines.forEach((target) => {
   const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
-  console.log(e)
+  // console.log(e)
 })
 
 function raf(time) {
@@ -101,16 +99,78 @@ ScrollTrigger.create({
   markers: true
 });
 
-document.querySelector("button").addEventListener("click", e => {
-  // scroll to the spot where .box-c is in the center.
-  // parameters: element, smooth, position
-  smoother.scrollTo(".smooth-content", true, "center center");
+// document.querySelector("button").addEventListener("click", e => {
+//   // scroll to the spot where .box-c is in the center.
+//   // parameters: element, smooth, position
+//   smoother.scrollTo(".smooth-content", true, "center center");
   
-  // or you could animate the scrollTop:
-  // gsap.to(smoother, {
-  //  scrollTop: smoother.offset(".box-c", "center center"),
-  //  duration: 1
-  // });
+//   // or you could animate the scrollTop:
+//   // gsap.to(smoother, {
+//   //  scrollTop: smoother.offset(".box-c", "center center"),
+//   //  duration: 1
+//   // });
+// });
+
+
+//FOR MARQUEE
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline();
+let marquee = document.querySelector(".marquee");
+let content = document.querySelector(".marquee__content");
+let items = document.querySelectorAll(".marquee__item");
+let tlLength = items.length * 2;
+
+// animation timeline
+tl.to(".marquee__content", tlLength, {
+  xPercent: -100,
+  repeat: -1,
+  ease: "none"
+});
+
+let currentScale = 1;
+let scaleTl;
+
+// scroll trigger
+ScrollTrigger.create({
+  markers: true,
+  trigger: marquee,
+  scrub: 1,
+  onUpdate: (self) => {
+    console.log(self.direction)
+    if (self.direction == 1) {
+      isUpdated = true;
+      let tScale = self.getVelocity() / 200;
+      if (tScale > currentScale) {
+        currentScale = tScale;
+        scaleTl && scaleTl.kill();
+        scaleTl = gsap
+          .timeline({
+            deafults: {
+              ease: "power2.out"
+            },
+            onComplete: () => {
+              currentScale = 1;
+              scaleTl.kill();
+            }
+          })
+          .to(tl, {
+            duration: 0.1,
+            timeScale: tScale,
+              ease: "power2.out"
+          })
+          .to(
+            tl,
+            {
+              timeScale: 1,
+              duration: 0.5,
+              ease: "none"
+            },
+            "+=0.3"
+          );
+      }
+    }
+  }
 });
 
 
