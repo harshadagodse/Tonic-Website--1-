c98ea85eece6694011f8bead61453fb5878d0b81
harshada <harshadagodse@staticgsm.com>
2024-03-26 17:17:14 +0530

    change in index.html, css, home-script

--- js/home-pageScript.js
+++ js/home-pageScript.js
@@ -39,44 +39,44 @@ window.addEventListener('scroll', function() {
 //   this.classList.remove('hover-slow');
 // });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const frameContainer = document.querySelector('.frame-container');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const frameContainer = document.querySelector('.frame-container');
 
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-      frameContainer.classList.add('scrolled');
-    } else {
-      frameContainer.classList.remove('scrolled');
-    }
-  });
-});
+//   window.addEventListener('scroll', function() {
+//     if (window.scrollY > 0) {
+//       frameContainer.classList.add('scrolled');
+//     } else {
+//       frameContainer.classList.remove('scrolled');
+//     }
+//   });
+// });
 
-gsap.config({ trialWarn: false });
-console.clear();
-gsap.registerPlugin(ScrollTrigger, SplitText);
-const split = new SplitText("p", { type: "lines" });
-console.log("split.lines", split.lines);
-split.lines.forEach((target) => {
-  gsap.to(target, {
-    backgroundPositionX: 0,
-    ease: "none",
-    scrollTrigger: {
-      trigger: target,
-      // markers: true,
-      scrub: 1,
-      start: "top center",
-      end: "bottom center"
-    }
+// gsap.config({ trialWarn: false });
+// console.clear();
+// gsap.registerPlugin(ScrollTrigger, SplitText);
+// const split = new SplitText("p", { type: "lines" });
+// console.log("split.lines", split.lines);
+// split.lines.forEach((target) => {
+//   gsap.to(target, {
+//     backgroundPositionX: 0,
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: target,
+//       // markers: true,
+//       scrub: 1,
+//       start: "top center",
+//       end: "bottom center"
+//     }
 
-  });
-});
+//   });
+// });
 
 
 
   const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
-  console.log(e)
+  // console.log(e)
 })
 
 function raf(time) {
@@ -87,36 +87,41 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollTrigger.normalizeScroll(true)
+gsap.registerPlugin(ScrollTrigger)
 
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
-  normalizeScroll: true
-});
+const splitTypes = document.querySelectorAll('.team-members')
 
-// pin box-c when it reaches the center of the viewport, for 300px
-ScrollTrigger.create({
-  trigger: ".smooth-content",
-  pin: true,
-  start: "center center",
-  end: "+=300",
-  markers: true
-});
+splitTypes.forEach((char,i) => {
 
-document.querySelector("button").addEventListener("click", e => {
-  // scroll to the spot where .box-c is in the center.
-  // parameters: element, smooth, position
-  smoother.scrollTo(".smooth-content", true, "center center");
-  
-  // or you could animate the scrollTop:
-  // gsap.to(smoother, {
-  //  scrollTop: smoother.offset(".box-c", "center center"),
-  //  duration: 1
-  // });
-});
+    const bg = char.dataset.bgColor
+    const fg = char.dataset.fgColor
+
+    const text = new SplitType(char, { types: 'chars'})
+
+    gsap.fromTo(text.chars, 
+        {
+            color: bg,
+        },
+        {
+            color: fg,
+            duration: 0.3,
+            stagger: 0.02,
+            scrollTrigger: {
+                trigger: char,
+                start: 'top 80%',
+                end: 'top 20%',
+                scrub: true,
+                markers: false,
+                toggleActions: 'play play reverse reverse'
+            }
+    })
+})
+
+
+
+
+
+
 
 
