af6afd2a5c79448dbff6f9b3cb3778dff202bed6
StaticGSM <gsm772@gmail.com>
2024-03-31 21:29:25 +0530

    mobile menu added

--- js/home-pageScript.js
+++ js/home-pageScript.js
@@ -5,6 +5,7 @@ window.addEventListener('scroll', function() {
   document.querySelector('.tonic-the-sound').style.animationDuration = animationDuration + 's';
 });
 
+//conact page scroll text
 window.addEventListener('scroll', function() {
   var scrollPosition = window.scrollY;
   var animationDuration = 20 + scrollPosition * 0.01; // Decrease the animation duration as you scroll down
@@ -116,12 +117,84 @@ splitTypes.forEach((char,i) => {
                 toggleActions: 'play play reverse reverse'
             }
     })
-})
+});
+
+//FOR MARQUEE
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline();
+let marquee = document.querySelector(".marquee");
+let content = document.querySelector(".marquee__content");
+let items = document.querySelectorAll(".marquee__item");
+let tlLength = items.length * 2;
+
+// animation timeline
+tl.to(".marquee__content", tlLength, {
+  xPercent: -100,
+  repeat: -1,
+  ease: "none"
+});
+
+let currentScale = 1;
+let scaleTl;
+
+// scroll trigger
+ScrollTrigger.create({
+  markers: false,
+  trigger: marquee,
+  scrub: 1,
+  onUpdate: (self) => {
+    console.log(self.direction)
+    if (self.direction == 1) {
+      isUpdated = true;
+      let tScale = self.getVelocity() / 200;
+      if (tScale > currentScale) {
+        currentScale = tScale;
+        scaleTl && scaleTl.kill();
+        scaleTl = gsap
+          .timeline({
+            deafults: {
+              ease: "power2.out"
+            },
+            onComplete: () => {
+              currentScale = 1;
+              scaleTl.kill();
+            }
+          })
+          .to(tl, {
+            duration: 0.1,
+            timeScale: tScale,
+              ease: "power2.out"
+          })
+          .to(
+            tl,
+            {
+              timeScale: 1,
+              duration: 0.5,
+              ease: "none"
+            },
+            "+=0.3"
+          );
+      }
+    }
+  }
+});
 
 
+//Menu
+var navigation = new TimelineLite({paused:true, reversed:true});
+navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
+          .to(".navbar", 0.3, {opacity: 0}, "-=0.1")
+          .to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
+          .from(".menu", 0.5, {opacity: 0, y: 30})
+          .from(".social", 0.5, {opacity: 0});
 
-
-
-
-
-
+$(".navbar, .close").click (function() {
+  console.log("clicked");
+  navigation.reversed() ? navigation.play() : navigation.reverse();
+  if ($('navigationWrap').hasClass('menu-open')) {
+    $('navigationWrap').removeClass('menu-open');
+  } else {
+    $('navigationWrap').addClass('menu-open');
+  }
+});
