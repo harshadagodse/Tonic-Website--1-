e37561fe6588a63e57dc826a50e90c4cc71950a3
StaticGSM <gsm772@gmail.com>
2024-04-12 17:59:58 +0530

    global menu for studio details

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+//Menu
+var navigation = new TimelineLite({paused:true, reversed:true});
+navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
+          .to(".navbar", 0.3, {opacity: 0}, "-=0.1")
+          .to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
+          .from(".menu", 0.5, {opacity: 0, y: 30})
+          .from(".social", 0.5, {opacity: 0});
+
+$(".navbar, .close").click (function() {
+  console.log("clicked");
+  navigation.reversed() ? navigation.play() : navigation.reverse();
+  if ($('navigationWrap').hasClass('menu-open')) {
+    $('navigationWrap').removeClass('menu-open');
+  } else {
+    $('navigationWrap').addClass('menu-open');
+  }
+});
